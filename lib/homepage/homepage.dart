11ca05cf72b6b92cea95a7b9cc5f2f0e0903cfb3
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-03-17 23:55:16 +0600

    Update Features

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,14 +1,146 @@
 import 'package:flutter/material.dart';
-class homepage extends StatelessWidget {
-  const homepage({super.key});
+
+import '../features/Offers.dart';
+import '../features/add balance.dart';
+import '../features/balance.dart';
+import '../features/payment.dart';
+import '../features/recharge.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: screenWidth,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                ),
+                child: ClipRRect(
+                  child: Image.asset(
+                    'asset/RM.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const Text(
+                'Recharge Mate',
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20), // Add some spacing between image and buttons
+          const Features(), // Add the features widget with buttons beneath the image
+        ],
+      ),
+    );
+  }
+}
+class Features extends StatelessWidget {
+  const Features({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: const Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MenuItem(title: 'Recharge', imagePath: 'asset/RE.png', page: recharge()),
+              MenuItem(title: 'Balance', imagePath: 'asset/RE.png', page: balance()),
+              MenuItem(title: 'Offers', imagePath: 'asset/RE.png', page: Offers()),
+              MenuItem(title: 'Payment', imagePath: 'asset/RE.png', page: payment()),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MenuItem(title: 'Add Balance', imagePath: 'asset/RE.png', page: addBalance()),
+               // Add Spacer widget to occupy remaining space
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 
+class MenuItem extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final Widget page;
 
+  const MenuItem({
+    required this.title,
+    required this.imagePath,
+    required this.page,
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        width: MediaQuery.of(context).size.width * 0.20,
+        height: MediaQuery.of(context).size.width * 0.20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: const [
+            /*BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 1,
+              blurRadius: 3,
+            ),*/
+          ],
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset(
+                imagePath,
+                width: double.infinity, // Match parent width
+                height: double.infinity, // Match parent height
+                // fit: BoxFit.cover, // Ensure the image fills the container
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
