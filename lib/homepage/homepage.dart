2a7c59111bebd0017fc937d7ea3a21915ee7ebe2
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-03-18 23:38:27 +0600

    Service Update

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -53,7 +53,19 @@ class HomePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20), // Add some spacing between image and buttons
-            const Features(), // Add the features widget with buttons beneath the image
+            const Features(),
+            const SizedBox(height: 20),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                '    Others',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18,fontWeight: FontWeight.w200),
+              ),
+            ),
+            const SizedBox(height: 20),
+            otherFeatures(),
+            // Add the features widget with buttons beneath the image
           ],
         ),
       ),
@@ -61,6 +73,7 @@ class HomePage extends StatelessWidget {
   }
 
 }
+//Services
 class Features extends StatelessWidget {
   const Features({Key? key}) : super(key: key);
 
@@ -97,14 +110,101 @@ class Features extends StatelessWidget {
     );
   }
 }
-
-
 class MenuItem extends StatelessWidget {
   final String title;
   final String imagePath;
   final Widget page;
 
   const MenuItem({
+    required this.title,
+    required this.imagePath,
+    required this.page,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        width: MediaQuery.of(context).size.width * 0.20,
+        height: MediaQuery.of(context).size.width * 0.20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          // color: Colors.white,
+          boxShadow: const [
+            /*BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 1,
+              blurRadius: 3,
+            ),*/
+          ],
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset(
+                imagePath,
+                width: double.infinity, // Match parent width
+                height: double.infinity, // Match parent height
+                // fit: BoxFit.cover, // Ensure the image fills the container
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Others
+class otherFeatures extends StatelessWidget {
+  const otherFeatures({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        // color: Colors.white,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: const Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              others(title: 'Recharge', imagePath: 'asset/RE.png', page:  Recharge()),
+              others(title: 'Balance', imagePath: 'asset/balance.png', page: balance()),
+
+              others(title: 'Offers', imagePath: 'asset/offers.png', page: Offers()),
+              others(title: 'Payment', imagePath: 'asset/pay.png', page: payment()),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+class others extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final Widget page;
+
+  const others({
     required this.title,
     required this.imagePath,
     required this.page,
