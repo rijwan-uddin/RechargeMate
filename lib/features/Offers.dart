94ace99a7cf05b4a1aa94d0f6d42a0db00c2584f
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-03-23 23:16:01 +0600

    Update

--- lib/features/Offers.dart
+++ lib/features/Offers.dart
@@ -1,22 +1,79 @@
 import 'package:flutter/material.dart';
+
 class Offers extends StatelessWidget {
-  const Offers({super.key});
+  const Offers({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Offers"),
+        title: const Text("Offers"),
         actions: [
           IconButton(
-            icon: Icon(Icons.notifications),
+            icon: const Icon(Icons.notifications),
             onPressed: () {
-// Handle notification button press
+              // Handle notification button press
             },
           ),
         ],
       ),
-
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Your Offers',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  SizedBox(width: 8),
+                  OfferButton(label: 'Minute'),
+                  SizedBox(width: 8),
+                  OfferButton(label: '  MB  '),
+                  SizedBox(width: 8),
+                  OfferButton(label: '  SMS '),
+                  SizedBox(width: 8),
+                  OfferButton(label: 'Combo'),
+                  SizedBox(width: 8),
+                  OfferButton(label: 'Bundle'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+class OfferButton extends StatelessWidget {
+  final String label;
+
+  const OfferButton({required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Handle button press
+      },
+      child: Text(label),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        primary: Colors.blue,
+        textStyle: const TextStyle(fontSize: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+}
+
